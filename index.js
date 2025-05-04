393dba6073706ffd9d4b202056b134ed2626ccff
Charli <charli.vannood@gmail.com>
2025-05-04 15:51:07 +0200

    buying and eating food

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mineflayer = require('mineflayer');
-const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
+const { pathfinder, goals } = require('mineflayer-pathfinder');
+const vec3 = require('vec3');
 
 // SOME CONFIG OPTIONS:
 const server_address = 'polydural.com'
@@ -19,6 +20,11 @@ var hit_messages = [
     "Please leave me alone :C",
     "Can I help you?!"
 ]
+var food_tags = [
+    "bread", "apple", "cookie", "beef"
+]
+
+var signsFound = {}
 
 const bot = mineflayer.createBot({
   host: server_address,
@@ -28,12 +34,82 @@ const bot = mineflayer.createBot({
 });
 bot.loadPlugin(pathfinder);
 
+let walking_to_point = false
+let buying_sign = false
 bot.on('spawn', () => {
     console.log('Bot has spawned.');
+    setInterval(() => {
+        let walk_to_sign = false
+        checkForSigns()
+        eatFood()
+        checkHunger()
+        if (walking_to_point) {
+            const distance = bot.entity.position.distanceTo(vec3(walking_to_point[0], walking_to_point[1], walking_to_point[2]));
+            if (distance < 3) {
+                if (buying_sign) {
+                    bot.activateBlock(bot.blockAt(vec3(walking_to_point[0], walking_to_point[1], walking_to_point[2])))
+                    buying_sign = false
+                }
+                walking_to_point = false
+            }
+        }
+        if (Math.random() > 0.9) walk_to_sign = true
+        if (walk_to_sign) {
+            if (Object.keys(signsFound).length == 0) return
+            let sign = Object.keys(signsFound)[Math.floor(Math.random() * Object.keys(signsFound).length)].split("x")
+            walking_to_point = sign
+            const goal = new goals.GoalNear(
+                sign[0], sign[1], sign[2], 1
+            );
+            bot.pathfinder.setGoal(goal);
+        } else {
+            if (walking_to_point) return
+            const goal = new goals.GoalNear(
+                bot.entity.position.x + (Math.random() * 50 - 25), 
+                bot.entity.position.y, 
+                bot.entity.position.z + (Math.random() * 50 - 25), 1
+            );
+            bot.pathfinder.setGoal(goal);
+        }
+    }, 2000)
 });
 
+async function eatFood() {
+    const foodItem = bot.inventory.items().find(item => food_tags.includes(item.name));
+    if (!foodItem) return
+
+    try {
+        await bot.equip(foodItem, 'hand');
+        await bot.consume();
+    } catch (err) {
+        console.log("Couldn't eat food: " + err.message)
+    }
+}
+
+function checkHunger() {
+    if (bot.food < 10) {
+        if (Object.keys(signsFound).length == 0) return
+
+        let sign_is_food = false
+        let signCoordinates;
+        for (let i = 0; i < 20; i++) {
+            if (sign_is_food) continue
+            signCoordinates = Object.keys(signsFound)[Math.floor(Math.random() * Object.keys(signsFound).length)].split("x")
+            let sign = signsFound[`${signCoordinates[0]}x${signCoordinates[1]}x${signCoordinates[2]}`]
+
+            for (let item = 0; item < food_tags.length; item++) {
+                if (sign["text"].includes(food_tags[item])) {
+                    sign_is_food = true
+                }
+            }
+        }
+
+        walking_to_point = signCoordinates
+        buying_sign = true
+    }
+}
+
 function checkForSigns() {
-    bot.chat(`Looking for signs`);
     const signs = bot.findBlocks({
         matching: (block) => { return block.name.includes('sign') },
         maxDistance: 10,
@@ -41,18 +117,23 @@ function checkForSigns() {
     });
 
     if (signs.length > 0) {
-        bot.chat(`Wow I see ${signs.length} signs`);
         signs.forEach(sign => {
             const block = bot.blockAt(sign);
             if (block && block.name.includes('sign')) {
-                const signText = block.getSignText()[0];
-                if (signText == "\n\n\n") return
-                console.log(signText)
+                const signText = block.getSignText()[0].replace(/\n/g, '#');
+                if (signText == "   ") return
+                if (!signText.includes("[buy]") && !signText.includes("[sell]")) return
+                let shopText = signText.split("#")
+                signsFound[`${sign.x}x${sign.y}x${sign.z}`] = {
+                    "type": shopText[0].replace("[", "").replace("]", ""),
+                    "text": shopText[1].toLowerCase() + " " + shopText[2].toLowerCase(),
+                    "price": shopText[3].replace("$", "")
+                }
             }
         });
-    } else {
-        bot.chat(`I see no signs :C`);
     }
+
+    console.log(signsFound)
 }
 
 var attackedBy = {}
@@ -99,6 +180,7 @@ bot.on('entityHurt', (entity) => {
 
 bot.on('message', (json_message) => {
     let message = json_message.toString().toLowerCase()
+    console.log(message)
     if (message.includes("/register")) {
         console.log(`I have to register, using password ${bot_password}`)
         bot.chat(`/register ${bot_password} ${bot_password}`);
